879cc8e53523699b2234a9a1b686fd6650df5c9e
Crupercio <Luis.f.cruzsanchez@outlook.com>
2024-09-05 14:47:27 -0700

    adding exiting option

--- calculator.py
+++ calculator.py
@@ -17,6 +17,7 @@ operations = {
     "*":multiply,
     "/":divide,
 }
+operations_symbol = ["+","-","*","/"]
 
 restart_calculator = True
 while restart_calculator:
@@ -26,11 +27,19 @@ while restart_calculator:
     while continue_calculations:
         print("+\n-\n*\n/")
         selected_key_operation = input("Pick an operation: ")
-        second_number = float(input("What's the next number?: "))
-        result = operations[selected_key_operation](first_number,second_number)
-        print(f"{first_number} {selected_key_operation} {second_number} = {result}")
-        next_operation = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
-        if next_operation == 'y':
-            first_number = result
-        if next_operation == 'n':
-            continue_calculations = False
+        if selected_key_operation in operations_symbol:
+            second_number = float(input("What's the next number?: "))
+            result = operations[selected_key_operation](first_number,second_number)
+            print(f"{first_number} {selected_key_operation} {second_number} = {result}")
+            next_operation = input(f"Type 'y' to continue calculating with {result}, type 'n' to start a new calculation, or 'x' to exit : ")
+            if next_operation == 'y':
+                first_number = result
+            elif next_operation == 'n':
+                continue_calculations = False
+            else:
+                restart_calculator = False
+                continue_calculations = False
+
+        else:
+            print("Wrong operator")
+
